dc1ba2f7775ad071e2ab684f815bc3246d6052de
Unknown <bartdebever112@gmail.com>
2019-06-21 12:48:57 +0200

    First implementation of the post preview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModules } from './material-modules';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { SideNavComponent } from './layout/side-nav/side-nav.component';
+import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
+import { PostComponent } from './pages/post/post.component';
+import { PostPreviewComponent } from './components/post-preview/post-preview.component';
+// Import FontAwesome icons where needed.
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    NavBarComponent,
+    PostComponent,
+    PostPreviewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModules,
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    library.add(faHeart);
+  }
+}
---
+++ src/app/models/post.ts
@@ -0,0 +1,7 @@
+export class Post {
+    title: string;
+    body: string;
+    bannerUrl: string;
+    author: string;
+    votes: number;
+}
