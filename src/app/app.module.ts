b5d4c91359d28f47c829c0d89332e4ad5578eab1
Bart de Bever <bartdebever112@gmail.com>
2019-07-31 23:43:29 +0200

    The like button is now a component (#10)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,28 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { AppComponent } from "./app.component";
-import { HttpClientModule } from "@angular/common/http";
-import { MaterialModules } from "./material-modules";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { SideNavComponent } from "./layout/side-nav/side-nav.component";
-import { NavBarComponent } from "./layout/nav-bar/nav-bar.component";
-import { PostComponent } from "./pages/post/post.component";
-import { ToastrModule } from "ngx-toastr";
-import { PostPreviewComponent } from "./components/post-preview/post-preview.component";
-import { AppRoutingModule } from "./app-routing.module";
-import { CreatePostComponent } from "./pages/create-post/create-post.component";
-import { LMarkdownEditorModule } from "ngx-markdown-editor";
-import { GameSelectorComponent } from "./components/game-selector/game-selector.component";
-import { FormsModule } from "@angular/forms";
-import { PostHelpComponent } from "./components/post-help/post-help.component";
-import { LoginComponent } from "./pages/login/login.component";
-import { AuthCallbackComponent } from "./auth-callback/auth-callback.component";
-import { MarkdownModule, MarkedOptions } from "ngx-markdown";
-import { PostDisplayComponent } from "./pages/post-display/post-display.component";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModules } from './material-modules';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { SideNavComponent } from './layout/side-nav/side-nav.component';
+import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
+import { PostComponent } from './pages/post/post.component';
+import { ToastrModule } from 'ngx-toastr';
+import { PostPreviewComponent } from './components/post-preview/post-preview.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CreatePostComponent } from './pages/create-post/create-post.component';
+import { LMarkdownEditorModule } from 'ngx-markdown-editor';
+import { GameSelectorComponent } from './components/game-selector/game-selector.component';
+import { FormsModule } from '@angular/forms';
+import { PostHelpComponent } from './components/post-help/post-help.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AuthCallbackComponent } from './auth-callback/auth-callback.component';
+import { MarkdownModule, MarkedOptions } from 'ngx-markdown';
+import { PostDisplayComponent } from './pages/post-display/post-display.component';
+import { LikeButtonComponent } from './components/like-button/like-button.component';
 // Import FontAwesome icons where needed.
 
 @NgModule({
@@ -36,7 +37,8 @@ import { PostDisplayComponent } from "./pages/post-display/post-display.componen
     PostHelpComponent,
     LoginComponent,
     AuthCallbackComponent,
-    PostDisplayComponent
+    PostDisplayComponent,
+    LikeButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/like-button/like-button.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { PostService } from 'src/app/services/post/post.service';
+import { PostText } from 'src/app/text/post.text';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-like-button',
+  templateUrl: './like-button.component.html',
+  styleUrls: ['./like-button.component.scss']
+})
+export class LikeButtonComponent implements OnInit {
+  /**
+   * The amount of likes the post has.
+   */
+  @Input() likes: number;
+
+  /**
+   * The id of the post.
+   */
+  @Input() postId: number;
+
+  /**
+   * If the post is liked by the current user.
+   */
+  @Input() liked: boolean;
+  constructor(
+    private postService: PostService,
+    private toastrService: ToastrService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  likePost() {
+    this.postService.likePost(this.postId).subscribe(() => {
+      if (this.liked) {
+        this.likes--;
+        this.liked = false;
+      } else {
+        this.likes++;
+        this.liked = true;
+      }
+    }, () => {
+      this.toastrService.error(PostText.failedPostLike);
+    });
+  }
+}
