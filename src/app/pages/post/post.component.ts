9204639243e06ca0d7db963c321d6efc668acf54
Bart de Bever <bartdebever112@gmail.com>
2019-08-01 12:46:19 +0200

    Added the ability to debug and search

--- src/app/pages/post/post.component.ts
+++ src/app/pages/post/post.component.ts
@@ -12,15 +12,16 @@ import { PostText } from 'src/app/text/post.text';
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
-
   constructor(
     private postService: PostService,
     private router: Router,
     private toastrService: ToastrService
-    ) { }
+  ) {}
   loading: boolean = true;
   posts: Post[];
   displayPosts: Post[];
+  searchTerm: string;
+  gameId: number = 0;
 
   ngOnInit() {
     const posts = this.postService.getPosts();
@@ -28,14 +29,16 @@ export class PostComponent implements OnInit {
       this.setupPosts(posts);
       this.loading = false;
     } else {
-      posts.subscribe(postArray => {
+      posts.subscribe(
+        postArray => {
           this.setupPosts(postArray);
           this.loading = false;
         },
         error => {
           this.loading = false;
           this.toastrService.error(PostText.failedPostsLoad);
-        });
+        }
+      );
     }
   }
 
@@ -45,12 +48,31 @@ export class PostComponent implements OnInit {
   }
 
   onGameChange(gameId: number): void {
-    if (gameId === 0) {
+    this.gameId = gameId;
+    this.filterPosts();
+  }
+
+  onSearchChange(searchTerm: string): void {
+    this.searchTerm = searchTerm;
+    this.filterPosts();
+  }
+
+  filterPosts(): void {
+    if (this.gameId === 0) {
       this.displayPosts = this.posts;
-      return;
+    } else {
+      this.displayPosts = this.posts.filter(
+        post => post.gameId === this.gameId
+      );
     }
 
-    this.displayPosts = this.posts.filter(post => post.gameId === gameId);
+    if (this.searchTerm) {
+      this.displayPosts = this.displayPosts.filter(
+        post =>
+          post.title.includes(this.searchTerm) ||
+          post.author.name.includes(this.searchTerm)
+      );
+    }
   }
 
   createPost() {
