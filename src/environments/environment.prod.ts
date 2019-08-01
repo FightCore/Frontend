e46777aba2b78ec169fa9e981bd92da5290c13dd
Bart de Bever <bartdebever112@gmail.com>
2019-08-01 16:33:47 +0200

    Added missing vars to prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: true,
-  mocking: false
+  mocking: false,
+  baseUrl: 'https://localhost:5001',
+  authentication: {
+    authUrl: 'http://localhost:5000',
+    callbackUrl: 'http://localhost:4200/auth-callback',
+    redirectUrl: 'http://localhost:4200',
+    clientId: 'angular_spa'
+  }
 };
