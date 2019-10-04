08443815262acdd41d7b510be2103c2c2487772b
Bart de Bever <bartdebever112@gmail.com>
2019-10-04 10:25:22 +0200

    Dynamic theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'FightCore';
+  constructor(public overlayContainer: OverlayContainer) { }
+  @HostBinding('class') componentCssClass;
+
+  ngOnInit(): void {
+    this.testTheme();
+  }
+
+  public testTheme() {
+    this.overlayContainer.getContainerElement().classList.add('melee-theme');
+    this.componentCssClass = 'melee-theme';
+  }
 }
