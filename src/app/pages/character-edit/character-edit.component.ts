9fd44106823ef735438f08f1fe625b191d594a6a
Bart de Bever <bartdebever112@gmail.com>
2019-11-01 13:34:24 +0100

    Improve the character edit page looks

--- src/app/pages/character-edit/character-edit.component.ts
+++ src/app/pages/character-edit/character-edit.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CharacterService } from 'src/app/services/character/character.service';
 import { Character } from 'src/app/models/character';
 import { NotablePlayer } from 'src/app/models/notablePlayer';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { StaticRoutes } from 'src/app/routes/static-routes';
 
 @Component({
   selector: 'app-character-edit',
@@ -18,7 +19,8 @@ export class CharacterEditComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private characterService: CharacterService) {
+    private characterService: CharacterService,
+    private router: Router) {
     }
 
   ngOnInit() {
@@ -42,6 +44,9 @@ export class CharacterEditComponent implements OnInit {
     if (index > -1) {
       this.character.notablePlayers.splice(index, 1);
     }
+  }
+  toCharacters(): void {
+    this.router.navigate([StaticRoutes.characters, this.character.id]);
   }
 
 }
