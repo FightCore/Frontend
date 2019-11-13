1d8f5c2e2e3babdd6044d42418cb5bf9b5dbf4f4
Bart de Bever <bartdebever112@gmail.com>
2019-11-13 21:41:29 +0100

    Tiny update

--- src/app/pages/tournaments/tournaments.component.ts
+++ src/app/pages/tournaments/tournaments.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TournamentService } from 'src/app/services/tournaments/tournament.service';
 import { Tournament } from 'src/app/models/tourrnament';
+import { IpService } from 'src/app/services/ip/ip.service';
 
 @Component({
   selector: 'app-tournaments',
@@ -9,9 +10,18 @@ import { Tournament } from 'src/app/models/tourrnament';
 })
 export class TournamentsComponent implements OnInit {
   tournaments: Tournament[];
-  constructor(private tournamentService: TournamentService) { }
+  latitude: number;
+  longitude: number;
+  useCountry: boolean;
+  constructor(
+    private tournamentService: TournamentService,
+    private ipService: IpService) { }
 
   ngOnInit() {
+    this.ipService.getIp().subscribe(ipResult => {
+      this.latitude = ipResult.lat;
+      this.longitude = ipResult.lon;
+    });
     this.tournamentService.getAll().subscribe(tournaments => {
       this.tournaments = tournaments.sort((a, b) => a.startAt > b.startAt ? 1 : -1);
       console.log(this.tournaments);
