0520d5988ee67d0d20e04a62308414ce6b415e8d
Bart de Bever <bartdebever112@gmail.com>
2020-04-28 19:55:48 +0200

    Move of components

--- src/app/pages/post/post.component.ts
+++ src/app/pages/post/post.component.ts
@@ -6,7 +6,7 @@ import { StaticRoutes } from 'src/app/routes/static-routes';
 import { ToastrService } from 'ngx-toastr';
 import { PostText } from 'src/app/text/post.text';
 import { UserOptions } from 'src/app/options/userOptions';
-import { CharacterPickerComponent } from 'src/app/components/character-picker/character-picker.component';
+import { CharacterPickerComponent } from 'src/app/components/characters/character-picker/character-picker.component';
 
 @Component({
   selector: 'app-post',
