6eebb32c373634609d207b89aface450a1d88e47
Bart de Bever <bartdebever112@gmail.com>
2020-08-11 11:33:24 +0200

    Changed default from nl to en.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,10 @@ export class AppComponent implements OnInit {
 
   constructor(translate: TranslateService) {
     // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('nl');
+    translate.setDefaultLang('en');
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use('nl');
+    translate.use('en');
   }
   ngOnInit(): void {}
 }
