cd014e7b7a0bc5fd5135a1b3d387bd0a361e6ba1
Bart de Bever <bartdebever112@gmail.com>
2020-10-14 19:22:06 +0200

    - Update packages to latest version - Add new look for the front page - Show post on edit screen

--- src/app/components/posts/editor/edit-intial-post/edit-intial-post.component.ts
+++ src/app/components/posts/editor/edit-intial-post/edit-intial-post.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { CDK_CONNECTED_OVERLAY_SCROLL_STRATEGY } from '@angular/cdk/overlay/overlay-directives';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Post } from 'src/app/models/post';
 
 @Component({
   selector: 'app-edit-intial-post',
@@ -7,6 +9,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./edit-intial-post.component.scss']
 })
 export class EditIntialPostComponent implements OnInit {
+  @Input() post: Post;
   formGroup: FormGroup;
 
   selectedCharacter: number;
@@ -19,5 +22,14 @@ export class EditIntialPostComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    if (this.post) {
+      this.formGroup.setValue({
+        title: this.post.title,
+        description: this.post.description
+      });
+
+      this.selectedGame = this.post.gameId;
+      this.selectedCharacter = this.post.characterId;
+    }
   }
 }
